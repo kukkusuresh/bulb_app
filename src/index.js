19fd721c545551f728fd87bcf49600e4e2cded88
kukkusuresh <kukkusuresh18@gmail.com>
2022-01-23 18:20:30 +0530

    added a comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//this is a comment
 
 ReactDOM.render(
   <React.StrictMode>
